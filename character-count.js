74d5eaa5f8b846b2178353039268b50828b2b422
pythonic1 <pythonic1@gmail.com>
2014-07-31 21:15:34 +0100

    Tabs to spaces

--- character-count.js
+++ character-count.js
@@ -1,49 +1,49 @@
 (function($){
 
-	$.fn.charcount = function(options) {
+  $.fn.charcount = function(options) {
 
-		var $this = this;
+    var $this = this;
 
-		var opts = $.extend({
-			labelClass : 'charcount',
-			labelStyle : 'float:right',
-		}, options );
+    var opts = $.extend({
+      labelClass : 'charcount',
+      labelStyle : 'float:right',
+    }, options );
 
-		function charcount() {
+    function charcount() {
 
-			$this.each(function() {
+      $this.each(function() {
 
-				var $elem      = $(this);
-				var $label     = $elem.siblings('label');
-				var $labelSpan = $label.find('.'+opts.labelClass);
-				var iLimit     = $elem.data('charcount');
-				var iUsed      = $elem.val().length;
+        var $elem      = $(this);
+        var $label     = $elem.siblings('label');
+        var $labelSpan = $label.find('.'+opts.labelClass);
+        var iLimit     = $elem.data('charcount');
+        var iUsed      = $elem.val().length;
 
-				if ( iUsed > iLimit) {
-					$elem.val( $elem.val().substring(0, iLimit) );
-					iUsed = iLimit;
-				}
+        if ( iUsed > iLimit) {
+          $elem.val( $elem.val().substring(0, iLimit) );
+          iUsed = iLimit;
+        }
 
-				if ($labelSpan.length === 0) {
-					$label
-						.append('<span class="' + opts.labelClass + '" style="' +
-						    opts.labelStyle + '">' + iUsed + ' / ' + iLimit + '</span>');
-				}
+        if ($labelSpan.length === 0) {
+          $label
+            .append('<span class="' + opts.labelClass + '" style="' +
+                opts.labelStyle + '">' + iUsed + ' / ' + iLimit + '</span>');
+        }
 
-				else {
-					$labelSpan.text( iUsed + ' / ' + iLimit);
-				}
+        else {
+          $labelSpan.text( iUsed + ' / ' + iLimit);
+        }
 
-			});
+      });
 
-		}
+    }
 
-		charcount();
+    charcount();
 
-		this.on('keyup focus blur', charcount);
+    this.on('keyup focus blur', charcount);
 
-		return this;
+    return this;
 
-	};
+  };
 
 })(jQuery);
